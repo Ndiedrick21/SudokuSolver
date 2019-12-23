e31325974fbae6fa99e8ccb5b94da8b63eb6815a
Ndiedrick21 <nathan.diedrick@gmail.com>
2019-12-22 21:45:14 -0600

    Finish Sudoku constructor

--- Sudoku.java
+++ Sudoku.java
@@ -5,13 +5,38 @@ public class Sudoku{
     private int[][] grid;
 
     public Sudoku(String filename){
+        grid = new int[3][3];
         File pFile = new File(filename);
         try{
             FileReader fin = new FileReader(pFile);
             BufferedReader bin = new BufferedReader(fin);
-            Scanner scan = new Scanner(bin.readLine());
+            Scanner scan;
+            String currRow = bin.readLine();
+            while(currRow!=null){
+                int row = 0;
+                scan = new Scanner(currRow);
+                while(scan.hasNext()){
+                    int col = 0;
+                    if(scan.hasNextInt()){
+                        grid[row][col] = scan.nextInt();
+                    }else{
+                        String curr = scan.next();
+                        if(!curr.equalsIgnoreCase("x")){
+                            throw new IOException("Incorrect file format");
+                        }
+                        grid[row][col] = 0;
+                    }
+                    col++;
+                }
+                row++;
+                currRow = bin.readLine();
+            }
         }catch(IOException e){
             e.printStackTrace();
+        }finally{
+            bin.close();
+            scan.close();
         }
     }
+
 }
