2dc523f80f44bd32c64fcf7ab3f9545e9b21e01a
Ndiedrick21 <nathan.diedrick@gmail.com>
2019-12-22 21:24:29 -0600

    Created and started Sudoku class

---
+++ Sudoku.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Sudoku{
+    private int[][] grid;
+
+    public Sudoku(String filename){
+        File pFile = new File(filename);
+        try{
+            FileReader fin = new FileReader(pFile);
+            BufferedReader bin = new BufferedReader(fin);
+            Scanner scan = new Scanner(bin.readLine());
+        }
+    }
+}
