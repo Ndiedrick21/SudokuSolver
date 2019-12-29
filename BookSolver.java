b84b0fbf74495c93c04aa15f2c01480c7800b82d
Ndiedrick21 <nathan.diedrick@gmail.com>
2019-12-29 13:54:46 -0600

    Added puzzles and a program to sovle the all the puzzles

---
+++ BookSolver.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class BookSolver {
+    static int MAX_PAGE_NUMBER = 45;
+    public static void main(String[] args){
+        LinkedList<Sudoku> puzzles = new LinkedList<>();
+        for(int i = 31; i<=MAX_PAGE_NUMBER; i++){
+            puzzles.add(new Sudoku("puzzles/p"+i+".txt"));
+        }
+        System.out.println("Done reading files");
+        SudokuSolver solver;
+        for(Sudoku s:puzzles){
+            solver = new SudokuSolver(s);
+            solver.solve();
+            System.out.println(s.getSolved());
+            //s.printPuzzle();
+        }
+        System.out.println("Solved "+puzzles.size()+" puzzles");
+    }
+}
