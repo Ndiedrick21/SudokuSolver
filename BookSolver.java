dec98c991ea1aff08f6fb5f0479aba6c1b179750
Ndiedrick21 <nathan.diedrick@gmail.com>
2019-12-29 19:43:15 -0600

    added more puzzles

--- BookSolver.java
+++ BookSolver.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 
 public class BookSolver {
-    static int MAX_PAGE_NUMBER = 55;
+    static int MAX_PAGE_NUMBER = 70;
     public static void main(String[] args){
         LinkedList<Sudoku> puzzles = new LinkedList<>();
         for(int i = 31; i<=MAX_PAGE_NUMBER; i++){
