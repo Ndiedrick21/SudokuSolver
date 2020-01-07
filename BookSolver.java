5b43358fd06c93c3cdd76da1c01361193f67432f
diedrick.nathan@uwlax.edu <diedrick.nathan@uwlax.edu>
2020-01-07 15:06:14 -0600

    Added ability to solve selected difficulty of puzzle

--- BookSolver.java
+++ BookSolver.java
@@ -1,14 +1,47 @@
 import java.util.LinkedList;
 
 public class BookSolver {
-    static int MAX_PAGE_NUMBER = 95;
+    static int MAX_PAGE_NUMBER = 105;
+
+    //Different Sections of the book
+    static final int EASY_START = 31;
+    static final int EASY_LAST = 104;
+    static final int TRICKY_START = 105;
+    static final int TRICKY_LAST = 198;
+    static final int TOUGH_START = 199;
+    static final int TOUGH_LAST = 234;
+    static final int DIABOLICAL_START = 235;
+    static final int DIABOLICAL_LAST = 250;
     public static void main(String[] args){
         LinkedList<Sudoku> puzzles = new LinkedList<>();
-        for(int i = 31; i<=MAX_PAGE_NUMBER; i++){
+        int start = EASY_START;
+        int end = MAX_PAGE_NUMBER;
+        if(args.length>0){
+            if(args[0].equalsIgnoreCase("easy")){
+                end = EASY_LAST;
+                System.out.println("Solving all EASY puzzles...");
+            }else if(args[0].equalsIgnoreCase("tricky")){
+                start = TRICKY_START;
+                end = TRICKY_LAST;
+                System.out.println("Solving all TRICKY puzzles...");
+            }else if(args[0].equalsIgnoreCase("tough")){
+                start = TOUGH_START;
+                end = TOUGH_LAST;
+                System.out.println("Solving all TOUGH puzzles...");
+            }else if(args[0].equalsIgnoreCase("diabolical")){
+                start = DIABOLICAL_START;
+                end = DIABOLICAL_LAST;
+                System.out.println("Solving all DIABOLICAL puzzles...");
+            }
+        }
+
+        //Read selected puzzles
+        for(int i = start; i<=end; i++){
             puzzles.add(new Sudoku("puzzles/p"+i+".txt"));
         }
         System.out.println("Done reading files");
         SudokuSolver solver;
+        //Solve selected puzzles
         for(Sudoku s:puzzles){
             solver = new SudokuSolver(s);
             solver.solve();
